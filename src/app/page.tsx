925eba0e20cdfd4a0fe62824a6e61f5189251960
Jakob Løkke Madsen <jakob@jakobloekkemadsen.com>
2023-11-28 14:30:35 +0100

    Fri leg med SVG

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,46 @@
+"use client";
+
+import { useState } from "react";
+import Headline from "./components/Headline";
+import * as Icons from "./icons/Icons";
+
+
 function Niko() {
-  return <div className="text-niko">Brok</div>
+  const [isVisible, setIsVisible] = useState(false);
+
+  return (
+    <div className="flex flex-col items-center gap-[11px] text-red-500 m-5 p-3 border-blue-300 border rounded-sm shadow-sm">
+      <h1 className="text-xl sm:text-2xl md:text-3xl font-bold">Welcome to this App</h1>
+      <button 
+        className="bg-gray-100 py-1 px-5 rounded shadow" 
+        onClick={() => setIsVisible(!isVisible)}
+      >
+        Toggle
+      </button>
+
+      {isVisible ? <p>Hello World</p> : null}
+    </div>
+  );
 }
 
 function Thomas() {
-  return <div>Thomas</div>
+  return <div>Thomas</div>;
 }
 
 function Lars() {
-  return <div>Lars er her</div>
+  return <div>Lars er her</div>;
 }
 
 function Anj() {
-  return <div>Hr. Jensen er awsome. Nyt batteri tak!!!</div>
+  return <div>Hr. Jensen er awsome. Nyt batteri tak!!!</div>;
 }
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      Hej FBG.
+      <Headline>Hejsa</Headline>
+
+      <Icons.Star color="orange" />
 
       <Niko />
       <Niko />
@@ -33,5 +56,5 @@ export default function Home() {
 
       <p>Hejsa</p>
     </main>
-  )
+  );
 }
