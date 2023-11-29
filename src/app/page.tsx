2674337beb701764dc299b48eda628fb71505738
Thomas Christensen <tdc@fbg.dk>
2023-11-29 12:33:27 +0100

    Live kodning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 function Niko() {
   return <div className="text-niko">Brok</div>
 }
@@ -11,10 +12,40 @@ function Lars() {
 }
 
 export default function Home() {
+=======
+"use client";
+
+import { useEffect, useState } from "react";
+import Headline from "./components/Headline";
+import Person from "./components/Person";
+
+export default function Home() {
+  type person = {
+    name: string,
+    isPresent: boolean
+  }
+
+  const togglePresent = (p: person[], index: number) => {
+    let pe = p;
+    pe[index].isPresent = !pe[index].isPresent;
+    setPersoner(pe);
+  };
+
+  const [personer, setPersoner] = useState<person[]>([
+    { name: 'Lars', isPresent: false },
+    { name: 'Anders', isPresent: false },
+    { name: 'Nicolai', isPresent: false },
+    { name: 'Rasmus', isPresent: false },
+    { name: 'Thomas', isPresent: false },
+    { name: 'Kjeldsen', isPresent: false }
+  ]);
+
+>>>>>>> Stashed changes
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      Hej FBG.
+      <Headline>FBG Medier</Headline>
 
+<<<<<<< Updated upstream
       <Niko />
       <Niko />
       <Niko />
@@ -24,6 +55,24 @@ export default function Home() {
 
       <Thomas />
       <Lars />
+=======
+      {personer.map((p: person, index) => {
+        return <Person 
+          key={index}
+          name={p.name} 
+          id={index} 
+          toggleTilmeldt={() => togglePresent(personer, index)} 
+          erTilmeldt={p.isPresent} 
+        />
+      })}
+
+      <ul className="mt-12 border-gray-500 border p-12">
+        <li>Navn</li>
+        <li>Navn</li>
+        <li>Navn</li>
+        <li>Navn</li>
+      </ul>
+>>>>>>> Stashed changes
     </main>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,14 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        nicolai: '#f00000',
+        thomas: '#0f0000',
+        anders: '#00f000',
+        rasmus: '#000f00',
+        kjeldsen: '#0000f0',
+        lars: '#00000f'
       }
     },
     colors: {
