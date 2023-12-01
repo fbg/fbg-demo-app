216cf2cbe77bd61be412a449481ecb64dbb6bbce
Skorpiworm <70314909+Skorpiworm@users.noreply.github.com>
2023-12-01 12:44:23 +0100

    blah blah

---
+++ src/app/thomas copy/page.tsx
@@ -0,0 +1,5 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+
+export default () => <><Link href='/'>Tilbage</Link> <main className="flex min-h-screen flex-col items-center p-24"><div>Thomas playground</div></main></>
