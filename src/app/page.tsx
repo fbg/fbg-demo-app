0442ec7edec4da2f78483f412330092f5ad2b783
Skorpiworm <70314909+Skorpiworm@users.noreply.github.com>
2023-12-01 12:34:30 +0100

    trying to fix vercel compile error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,19 @@ import Link from "next/link";
 
 export default function Home() {
 
-  const [personer, setPersoner] = useState<person[]>([
-    { name: 'Lars', isPresent: false },
-    { name: 'Anders', isPresent: false },
-    { name: 'Rasmus', isPresent: false },
-    { name: 'Thomas', isPresent: false },
+  const [personer] = Array([
+    'Anders' ,
+    'Thomas' ,
+    'Lars' ,
+    'Rasmus' ,
   ]);
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
 
-      {personer.map((p, index) => {
+      {personer.map((name, index) => {
         return (<div className="flex gap-[5px] m-2 p-3 border-black border rounded-sm shadow-sm">
-          <Link href={p.name.toLowerCase()}>{p.name} playground</Link>
+          <Link href={name.toLowerCase()}>{name} playground</Link>
         </div>)
       })}
 
