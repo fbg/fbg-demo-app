6df7c50e9c607ea4e22f383b294a0f2e9cf95dac
Lars Andersen <lan@fbg.dk>
2023-12-21 11:24:23 +0100

    Added some animationwhen toggling attendants on/off

--- src/app/components/employeeList.tsx
+++ src/app/components/employeeList.tsx
@@ -25,11 +25,15 @@ const EmployeeList: React.FC<EmployeeListProps> = ({ employees }) => {
         }));
     };
 
+    const totalCount = employees.length;
+  
+    const ulPaddingBottomValue = (totalCount - 1) * 20 + 50;
+
     return (
     <div className="flex justify-center items-center">
-        <ul className="flex flex-col bg-white border-2 border-black rounded-lg p-12 m-12 content-between gap-4">
-        {employees.sort((a, b) => (attendance[a.id] > attendance[b.id] ? -1 : (attendance[a.id] < attendance[b.id] ? 1 : (a.name < b.name ? -1 : 1)))).map((employee) => (
-            <li className={`flex items-center p-[15px] ${ attendance[employee.id] ? 'bg-green-100' : 'bg-red-100' } text-black rounded`} key={employee.id}>
+        <ul className="flex flex-col bg-white border-2 border-black rounded-lg pt-[50px] pl-[50px] pr-[50px] m-12 content-between gap-4" style={{paddingBottom: `${ulPaddingBottomValue}px`}}>
+        {employees.sort((a, b) => (attendance[a.id] > attendance[b.id] ? -1 : (attendance[a.id] < attendance[b.id] ? 1 : (a.name < b.name ? -1 : 1)))).map((employee, index) => (
+            <li key={employee.id} className={`transition-all duration-200 flex items-center p-[15px] ${ attendance[employee.id] ? 'bg-green-100' : 'bg-red-100' } text-black rounded`} style={{ transform: `translateY(${index * 20}px)` }}>
                 <div className="flex-shrink-0">
                     <div className="flex my-auto px-[10px]">
                         {employee.name} - {employee.position}
