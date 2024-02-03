729637df7007bc2aa82d6deada9a3ca11983730f
Jakob Løkke Madsen <jakob@jakobloekkemadsen.com>
2024-02-03 16:01:18 +0100

    Add Switch example

--- src/app/anders/page.tsx
+++ src/app/anders/page.tsx
@@ -1,44 +1,53 @@
 "use client";
 import {Cross2Icon} from '@radix-ui/react-icons';
-import {Button, Dialog} from "@radix-ui/themes";
+import {Button, Dialog, Flex, Switch, Text} from "@radix-ui/themes";
 
 Anders.displayName = "Anders";
 
 export default function Anders() {
     return (
-        <Dialog.Root>
-        <Dialog.Trigger>
-          <Button className="Button violet">Edit profile</Button>
-        </Dialog.Trigger>
-          <Dialog.Content className="DialogContent">
-            <Dialog.Title className="DialogTitle">Edit profile</Dialog.Title>
-            <Dialog.Description className="DialogDescription">
-              Make changes to your profile here. Click save when you're done.
-            </Dialog.Description>
-            <fieldset className="Fieldset">
-              <label className="Label" htmlFor="name">
-                Name
-              </label>
-              <input className="Input" id="name" defaultValue="Pedro Duarte" />
-            </fieldset>
-            <fieldset className="Fieldset">
-              <label className="Label" htmlFor="username">
-                Username
-              </label>
-              <input className="Input" id="username" defaultValue="@peduarte" />
-            </fieldset>
-            <div style={{ display: 'flex', marginTop: 25, justifyContent: 'flex-end' }}>
-              <Dialog.Close>
-                <Button className="Button green">Save changes</Button>
-              </Dialog.Close>
-            </div>
-            <Dialog.Close>
-              <Button className="IconButton" aria-label="Close">
-                <Cross2Icon />
-              </Button>
-            </Dialog.Close>
-          </Dialog.Content>
-      </Dialog.Root>
+        <div>
+
+            <Text as="label" size="2">
+                <Flex gap="2">
+                    <Switch defaultChecked /> Sync settings
+                </Flex>
+            </Text>
+
+            <Dialog.Root>
+                <Dialog.Trigger>
+                    <Button className="Button violet">Edit profile</Button>
+                </Dialog.Trigger>
+                <Dialog.Content className="DialogContent">
+                    <Dialog.Title className="DialogTitle">Edit profile</Dialog.Title>
+                    <Dialog.Description className="DialogDescription">
+                        Make changes to your profile here. Click save when you're done.
+                    </Dialog.Description>
+                    <fieldset className="Fieldset">
+                        <label className="Label" htmlFor="name">
+                            Name
+                        </label>
+                        <input className="Input" id="name" defaultValue="Pedro Duarte"/>
+                    </fieldset>
+                    <fieldset className="Fieldset">
+                        <label className="Label" htmlFor="username">
+                            Username
+                        </label>
+                        <input className="Input" id="username" defaultValue="@peduarte"/>
+                    </fieldset>
+                    <div style={{display: 'flex', marginTop: 25, justifyContent: 'flex-end'}}>
+                        <Dialog.Close>
+                            <Button className="Button green">Save changes</Button>
+                        </Dialog.Close>
+                    </div>
+                    <Dialog.Close>
+                        <Button className="IconButton" aria-label="Close">
+                            <Cross2Icon/>
+                        </Button>
+                    </Dialog.Close>
+                </Dialog.Content>
+            </Dialog.Root>
+        </div>
 
     )
 }
